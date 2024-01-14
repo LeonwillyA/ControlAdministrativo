6a5d6d12aba12fd8934dc26dee7f87451dd07b90
LeonwillyA <leonalvaradowilly@gmail.com>
2024-01-13 22:53:57 -0500

    Actualización del proceso de autentificación de usuarios, con esto siento las bases para poder utilizar persistencia de datos, con los querys respectivos

--- src/main/java/uimenu/MenuUI.java
+++ src/main/java/uimenu/MenuUI.java
@@ -1,11 +1,20 @@
 package uimenu;
 
+import modelos.Administrador;
+import modelos.Conserje;
 import modelos.Propietario;
+import modelos.Vigilante;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MenuUI {
+    public static Administrador administradorLogeado;
+    public static Conserje conserjeLogeado;
+    public static Vigilante vigilanteLogeado;
+    public static  Propietario propietarioLogeado;
+
+
     public static mostrarMenu(){
         System.out.println("Administración de Actividades");
         System.out.println("Seleccione el tipo de usuario al cual perteneces: ");
@@ -24,17 +33,22 @@ public class MenuUI {
                 case 1:
                     System.out.println("Bienvenido propietario");
                     response = 0;
-                    mostrarPropietarioMenu();
-
+                    autenticarHumano(1);
                 break;
                 case 2:
                     System.out.println("Bienvenido Conserje");
+                    response = 0;
+                    autenticarHumano(2);
                     break;
                 case 3:
                     System.out.println("Bienvenido Vigilante");
+                    response = 0;
+                    autenticarHumano(3);
                     break;
                 case 4:
                     System.out.println("Bienvenido Administrador");
+                    response = 0;
+                    autenticarHumano(4);
                     break;
                 case 0:
                     System.out.println("Gracias por visitarnos");
@@ -51,9 +65,50 @@ public class MenuUI {
         // humanType = 3 Vigilante
         // humanType = 4 Administrador
         // Vamos hacer una lista en un array como prueba, para posteriormente utilizar una base de datos
-        ArrayList<Propietario> propietarios = new ArrayList<>();
-        propietarios.add(new Propietario("","",""));
+        ArrayList<Conserje> conserjes  = new ArrayList<>();
+        System.out.println(new Conserje("Jose","Rivadeneyra","jri@mail"));
+        System.out.println(new Conserje("Jaime","Canchari","jca@mail.com"));
+        System.out.println(new Conserje("Carlos","Padilla","cpa@mail.com"));
+        //Es como si fuera a mi capa de persistencia de datos y me trajera todos los usuarios que tengo
+        // registrado, dependiento del tipo de usuario que sea.
+        ArrayList<Administrador> administradors = new ArrayList<>();
+        System.out.println(new Administrador("Carlos","Urriate","curr@mail.com"));
+        System.out.println(new Administrador("Willy","Leóni","wle@mail.com"));
+
+        boolean emailCorrect = false;
+        do {
+            System.out.println("Inserta tu correo electronico: ");
+            Scanner sc = new Scanner(System.in);
+            String email = sc.nextLine();
+            if (humanType == 1){
+                for (){if ()}
+            }
+            if (humanType == 2){
+                for (Conserje c:conserjes ){
+                    if (c.getMail().equals(email)){
+                    emailCorrect = true;
+                    conserjeLogeado = c;
+                    mostrarConserjeMenu();
+                    //Indicar la conformidad de que se ha logeado
+                }}
+            }
+            if (humanType == 3){
+                for (){if ()}
+            }
+            if (humanType == 4){
+                for (Administrador a: administradors){
+                    if (a.getMail().equals(email)){
+                        emailCorrect = true;
+                        administradorLogeado = a;
+                        mostrarAdministradorMenu();
+                    }}}
+        }while (emailCorrect=true){}
+
     }
 
     static void mostrarPropietarioMenu(){}
+    static void mostrarConserjeMenu(){}
+    static void mostrarVigilanteMenu(){}
+    static void mostrarAdministradorMenu(){}
+
 }
