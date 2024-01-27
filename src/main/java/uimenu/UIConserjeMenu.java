6c56750f5dd3c2c1d48e5b0e4ee9d33166b25f61
LeonwillyA <leonalvaradowilly@gmail.com>
2024-01-26 22:28:42 -0500

    Se necesita organizar bien el esquema del menu del conserje, debido a que falta visualizar la parte en donde observa la tarea que va a realizar

--- src/main/java/uimenu/UIConserjeMenu.java
+++ src/main/java/uimenu/UIConserjeMenu.java
@@ -6,10 +6,11 @@ public class UIConserjeMenu {
     public static void mostrarMenuConserje(){
         int response=0;
         do{
+            System.out.println("\n \n ");
             System.out.println(" :: TAREAS PROGRAMADAS :: ");
+            System.out.println(" Un gusto poder atenderte " + MenuUI.conserjeLogeado);
             System.out.println("1. Visualizar las tareas");
             System.out.println("2. Dar OK a las tareas realizadas ");
-
             System.out.println("0. Return");
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
@@ -19,9 +20,17 @@ public class UIConserjeMenu {
             } else if (response == 2) {
                 System.out.println("Indicar las tareas que le daras ok: ");
             } else if (response == 0) {
-                mostrarMenuConserje();
+                MenuUI.mostrarMenu();
             }
         }
         while (response != 0);
+    }
+
+    private static void mostrarTareasOrdenadasAdministrador(){
+        int response = 0;
+
+        do {
+            System.out.println("Las tareas a realizar se muestra a continuación: ");
+        } while (response != 0);
     }
 }
